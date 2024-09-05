884e0ea00acd31b85410d868faa4d4bc7d690d60
tomasli <129140875+looksimplers@users.noreply.github.com>
2024-09-05 01:48:27 +0000

    rewrite signuppage

--- js/src/forum/components/RedirectToHomeAndOpenModalPage.js
+++ js/src/forum/components/RedirectToHomeAndOpenModalPage.js
@@ -1,27 +1,10 @@
 import app from 'flarum/forum/app';
 import Page from 'flarum/common/components/Page';
-import { extend } from 'flarum/common/extend';
-import Stream from 'flarum/common/utils/Stream';
-import SignUpModal from 'flarum/forum/components/SignUpModal';
 
 
 export default class RedirectToHomeAndOpenModalPage extends Page {
   oninit(vnode) {
     super.oninit(vnode);
-    extend(SignUpModal.prototype, 'fields', function (fields) {
-      const isOptional = app.forum.data.attributes['fof-doorman.allowPublic'];
-      const placeholder = isOptional
-        ? app.translator.trans('fof-doorman.forum.sign_up.doorman_placeholder_optional')
-        : app.translator.trans('fof-doorman.forum.sign_up.doorman_placeholder');
-      const params = m.route.param()
-      this.doorkey = Stream(params.doorkey) || Stream('');
-      fields.add(
-        'doorkey',
-        <div className="Form-group">
-          <input className="FormControl" name="fof-doorkey" type="text" placeholder={placeholder} bidi={this.doorkey} disabled={this.loading} />
-        </div>
-      );
-    });
 
     m.route.set('/');
 
--- js/src/forum/components/SignupPage.js
+++ js/src/forum/components/SignupPage.js
@@ -1,13 +1,37 @@
 import app from 'flarum/forum/app';
 import RedirectToHomeAndOpenModalPage from './RedirectToHomeAndOpenModalPage';
 import SignUpModal from 'flarum/forum/components/SignUpModal';
+import { extend } from 'flarum/common/extend';
+import Stream from 'flarum/common/utils/Stream';
 
+export default class SignupPage extends Page {
+  oninit(vnode) {
+    super.oninit(vnode);
+    extend(SignUpModal.prototype, 'fields', function (fields) {
+      const isOptional = app.forum.data.attributes['fof-doorman.allowPublic'];
+      const placeholder = isOptional
+        ? app.translator.trans('fof-doorman.forum.sign_up.doorman_placeholder_optional')
+        : app.translator.trans('fof-doorman.forum.sign_up.doorman_placeholder');
+      this.doorkey = Stream(app.doorkey) || Stream('');
+      fields.add(
+        'doorkey',
+        <div className="Form-group">
+          <input className="FormControl" name="fof-doorkey" type="text" placeholder={placeholder} bidi={this.doorkey} disabled={this.loading} />
+        </div>
+      );
+    });
 
-export default class SignupPage extends RedirectToHomeAndOpenModalPage {
+    m.route.set('/');
+
+    setTimeout(() => app.modal.show(this.createModal()), 1500);
+  }
   createModal() {
     if (!app.session.user) {
       
       return SignUpModal;
     }
+  }
+  view() {
+    return <div />;
   }
 }
